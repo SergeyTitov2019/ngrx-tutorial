811733215ee31bc220a794a94aebf5620d77f01a
Sergey Titov <sergeyntitov@gmail.com>
2022-08-01 20:33:49 +0300

    durum & ngrx are connected [ 002 ]

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { NgrxTrainingComponent } from "./ngrx-training/ngrx-training.component";
 import { DurumComponent } from "./durum/durum.component";
+import { StoreModule } from '@ngrx/store';
+import { reducers, metaReducers } from './reducers';
 
 @NgModule({
   declarations: [
@@ -12,7 +14,10 @@ import { DurumComponent } from "./durum/durum.component";
     DurumComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    StoreModule.forRoot(reducers, {
+      metaReducers
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
